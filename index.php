e75dfe09f12ac809eca1a22e3862be25c1222423
Amr Ebaid <aebaid@purdue.edu>
2018-06-04 17:41:50 -0400

    db configs

--- index.php
+++ index.php
@@ -1,26 +1,32 @@
 <?php
-	$host = "us-cdbr-iron-east-04.cleardb.net";
-	$user = "b84db3a0b45258";
-	$pass = "350fabd5";
-	$database = "heroku_6d357e957b92f91";
+    $cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $host = $cleardb_url["host"];
+    $user = $cleardb_url["user"];
+    $password = $cleardb_url["pass"];
+    $databsae = substr($cleardb_url["path"],1);
 
-    $linkID = mysql_connect($host, $user, $pass);
+    echo $host;
+    echo $user;
+    echo $password;
+    echo $database;
 
-	if($linkID){
-		mysql_set_charset('utf8');
-
-		mysql_select_db($database, $linkID);
-
-		$query = "SELECT * FROM `qusasat` AS q,`categories` AS c "
-			. "WHERE q.category=c.id ORDER BY RAND() LIMIT 0,1";
-		$resultID = mysql_query($query, $linkID);
-		$row = mysql_fetch_assoc($resultID);
-
-		$qusasa = $row['qusasa'];
-		$category = $row['category'];
-	} else {
-		$qusasa = "مصر غارقة في دوامة: كيف أعمل وأنت لا تعطيني مالاً؟.. كيف أعطيك مالاً وأنت لا تعمل؟";
-		$category = "قصاصات عن السياسة";
+    // $linkID = mysql_connect($host, $user, $password);
+    //
+	// if($linkID){
+	// 	mysql_set_charset('utf8');
+    //
+	// 	mysql_select_db($database, $linkID);
+    //
+	// 	$query = "SELECT * FROM `qusasat` AS q,`categories` AS c "
+	// 		. "WHERE q.category=c.id ORDER BY RAND() LIMIT 0,1";
+	// 	$resultID = mysql_query($query, $linkID);
+	// 	$row = mysql_fetch_assoc($resultID);
+    //
+	// 	$qusasa = $row['qusasa'];
+	// 	$category = $row['category'];
+	// } else {
+	// 	$qusasa = "مصر غارقة في دوامة: كيف أعمل وأنت لا تعطيني مالاً؟.. كيف أعطيك مالاً وأنت لا تعمل؟";
+	// 	$category = "قصاصات عن السياسة";
 	// }
 ?>
 
