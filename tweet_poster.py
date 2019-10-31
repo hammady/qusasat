641100b2bc3283c19a1361079442f78a4f8c9d30
Hossam Hammady <github@hammady.net>
2019-10-31 22:13:03 +0300

    Make shortest tweet at the end, and borrow from previous if necessary

--- tweet_poster.py
+++ tweet_poster.py
@@ -42,7 +42,7 @@ class TweetPoster():
         quotes = []
         current_quote = ""
         max_length = self._max_tweet_length - len(self._ellipsis) * 2
-        remaining_length = max_length - len(hashtag) - len('\n')
+        remaining_length = max_length
         for word in quote['quote'].split(): # no arguments split on whitespaces
             spaced_word = ' ' + word if current_quote != "" else word
             if len(spaced_word) < remaining_length:
@@ -53,12 +53,44 @@ class TweetPoster():
                 current_quote = self._ellipsis + word
                 remaining_length = max_length - len(current_quote)
         quotes.append({'quote': current_quote})
-        quotes[0]['hashtag'] = hashtag
+        return self._append_hashtag(quotes, hashtag)
+
+    def _append_hashtag(self, quotes, hashtag):
+        if not quotes:
+            return quotes
+        last_quote_text = quotes[-1]['quote']
+        last_quote_length = len(last_quote_text) + len('\n') + len(hashtag)
+        # if last quote is too long, split it into 2 quotes
+        if last_quote_length > self._max_tweet_length:
+            words = last_quote_text.split()
+            quotes.append({})
+            return self._distribute_words(quotes, words, hashtag)
+        # else if last quote is too short, borrow the previous quote if any and split both into 2 quotes
+        elif len(quotes) > 1 and last_quote_length < self._max_tweet_length // 2:
+            # remove the ellipsis from the end of previous quote [:-1] and start of last quote [1:]
+            words = quotes[-2]['quote'][:-1].split() + last_quote_text[1:].split()
+            return self._distribute_words(quotes, words, hashtag)
+        # last quote is not too long or too short, or there is only 1 quote that can fit the hashtag
+        else:
+            quotes[-1]['hashtag'] = hashtag
         return quotes
+
+    def _distribute_words(self, quotes, words, hashtag):
+        # put the first half in the previous quote
+        quotes[-2]['quote'] = self._join_first_half(words) + self._ellipsis
+        # put the second half in the last quote with the hashtag
+        quotes[-1] = {'quote': self._ellipsis + self._join_second_half(words), 'hashtag': hashtag}
+        return quotes
+
+    def _join_first_half(self, words):
+        return ' '.join(words[:len(words)//2])
+
+    def _join_second_half(self, words):
+        return ' '.join(words[len(words)//2:])
 
     def _format_quote(self, quote):
         if quote.get('hashtag'):
-            return '{}\n{}'.format(quote['hashtag'], quote['quote'])
+            return '{}\n{}'.format(quote['quote'], quote['hashtag'])
         else:
             return quote['quote']
 
